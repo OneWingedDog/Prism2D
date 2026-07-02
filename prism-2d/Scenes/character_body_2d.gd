a63d3c8868f93bd65cc1e118077c8b26da97e34c
onewingd <138161020+OneWingedDog@users.noreply.github.com>
2026-07-01 17:58:07 -0700

    added flying

--- prism-2d/Scenes/character_body_2d.gd
+++ prism-2d/Scenes/character_body_2d.gd
@@ -1,11 +1,14 @@
 extends CharacterBody2D
 @export var speed = 400
+var airborne: bool = false
 
 func get_input():
-	var input_direction = Input.get_vector("Left", "Right", "ui_text_backspace", "Jump")
-	velocity = input_direction * speed
+	var input_direction: Vector2 = Input.get_vector("Left", "Right", "ui_text_backspace", "Jump")
+	velocity.x = input_direction.x * speed
 	print(input_direction)
-
+	if Input.is_action_just_pressed("Jump"):
+		velocity.y = -200
+	
 func _physics_process(delta):
 	get_input()
 	move_and_slide()
