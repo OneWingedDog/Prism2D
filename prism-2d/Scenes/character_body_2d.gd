88a50049a9d2d8c13368ab7de37e2a109b9082ba
Idkxname248 <dkxname345@gmail.com>
2026-07-01 21:29:19 -0500

    Basic acell

--- prism-2d/Scenes/character_body_2d.gd
+++ prism-2d/Scenes/character_body_2d.gd
@@ -1,15 +1,29 @@
 extends CharacterBody2D
-@export var speed = 800
+var speedmax = 800
+@onready var speed = 800
 var airborne: bool = false
 var jumpv = -852
 @onready var inum = 0
 var inum_max = 50
 var jump:bool 
 var terminal_velocity:int = 10000
+@onready var oldinpt
+@onready var input_direction: Vector2 
+@onready var incnum:float = 0
 
 func get_input():
-	var input_direction: Vector2 = Input.get_vector("Left", "Right", "ui_text_backspace", "Jump")
-	velocity.x = input_direction.x * speed
+	
+	oldinpt = input_direction
+	
+	
+	
+	input_direction = Input.get_vector("Left", "Right", "ui_text_backspace", "Jump")
+	velocity.x = input_direction.x * (speed * incnum)
+	
+	if not oldinpt == input_direction:
+		incnum = 0
+	
+	
 	if Input.is_action_just_pressed("Jump") and jump == false:
 		inum = 0
 		velocity.y = jumpv 
@@ -18,10 +32,16 @@ func get_input():
 	velocity.y = velocity.y + inum
 
 func _physics_process(delta):
+	
+	if not incnum > 0.9:
+		incnum = incnum + 0.025
+	else:
+		incnum = 1
+	
 	if velocity.y == 0:
 		jump = false
 
-	print(velocity.y)
+	
 
 	inum = inum + 3
 
