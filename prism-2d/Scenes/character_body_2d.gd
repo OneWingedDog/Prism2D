f01555fdc3663d05c33d2bdf437ad9f9dbabea45
Idkxname248 <dkxname345@gmail.com>
2026-07-01 20:39:04 -0500

    Jumping finally actually fully works

--- prism-2d/Scenes/character_body_2d.gd
+++ prism-2d/Scenes/character_body_2d.gd
@@ -1,5 +1,5 @@
 extends CharacterBody2D
-@export var speed = 400
+@export var speed = 800
 var airborne: bool = false
 var jumpv = -852
 @onready var inum = 0
@@ -10,20 +10,26 @@ var terminal_velocity:int = 10000
 func get_input():
 	var input_direction: Vector2 = Input.get_vector("Left", "Right", "ui_text_backspace", "Jump")
 	velocity.x = input_direction.x * speed
-	if Input.is_action_just_pressed("Jump"):
+	if Input.is_action_just_pressed("Jump") and jump == false:
 		inum = 0
 		velocity.y = jumpv 
 		jump = true 
 	if jump == true:
 		velocity.y = velocity.y + inum
+
 func _physics_process(delta):
-	print(inum)
+	if velocity.y == 0:
+		jump = false
+
+	print(velocity.y)
+
 	inum = inum + 3
+
 	if position.y > 12000:
 		global_position = Vector2(0,0)
 	if inum > inum_max:
 		inum = inum_max
-		
+
 	if velocity.y > terminal_velocity:
 		velocity.y = terminal_velocity
 	get_input()
